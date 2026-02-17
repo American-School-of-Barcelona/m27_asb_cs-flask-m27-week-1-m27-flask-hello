ee0ff25eb5824bf096f2afcf90e1ce8c06f66383
Andrew Carle <105776495+mrcarle-asb@users.noreply.github.com>
2026-02-17 12:34:07 +0000

    root and about

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 # TODO: Add your routes below this line
 
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
